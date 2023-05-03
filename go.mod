d16685b74bbbe69e9406cb2b964da21b253760d6
Roman Timashev <roman@tmshv.ru>
2023-05-03 16:23:11 +0530

    add fiber

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.20
 require (
 	github.com/JohannesKaufmann/html-to-markdown v1.4.0 // indirect
 	github.com/PuerkitoBio/goquery v1.8.1 // indirect
+	github.com/andybalholm/brotli v1.0.5 // indirect
 	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/cixtor/readability v1.0.0 // indirect
 	github.com/gilliek/go-opml v1.0.0 // indirect
+	github.com/gofiber/fiber/v2 v2.44.0 // indirect
 	github.com/golang-migrate/migrate/v4 v4.15.2 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gosimple/slug v1.13.1 // indirect
@@ -15,12 +17,25 @@ require (
 	github.com/hashicorp/errwrap v1.1.0 // indirect
 	github.com/hashicorp/go-multierror v1.1.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.16.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.18 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16 // indirect
 	github.com/mmcdole/gofeed v1.2.1 // indirect
 	github.com/mmcdole/goxpp v1.1.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/philhofer/fwd v1.1.2 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
+	github.com/savsgio/dictpool v0.0.0-20221023140959-7bf2e61cea94 // indirect
+	github.com/savsgio/gotils v0.0.0-20230208104028-c358bd845dee // indirect
+	github.com/tinylib/msgp v1.1.8 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasthttp v1.45.0 // indirect
+	github.com/valyala/tcplisten v1.0.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.9.0 // indirect
+	golang.org/x/sys v0.7.0 // indirect
 	golang.org/x/text v0.9.0 // indirect
 )
--- main.go
+++ main.go
@@ -17,6 +17,7 @@ import (
 	"time"
 
 	"github.com/gilliek/go-opml/opml"
+	"github.com/gofiber/fiber/v2"
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite3"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
@@ -26,6 +27,7 @@ import (
 	"github.com/cixtor/readability"
 	"github.com/gosimple/slug"
 	"github.com/mmcdole/gofeed"
+	jsonfeed "github.com/mmcdole/gofeed/json"
 
 	md "github.com/JohannesKaufmann/html-to-markdown"
 )
@@ -278,20 +280,43 @@ func updatePageContent(db *sql.DB, page *Page, content string) error {
 	if err != nil {
 		return err
 	}
-    res, err := stmt.Exec(content, page.Url, page.CreatedAt)
+	res, err := stmt.Exec(content, page.Url, page.CreatedAt)
 	if err != nil {
 		return err
 	}
 
-    x, err := res.RowsAffected()
+	x, err := res.RowsAffected()
 	if err != nil {
 		return err
 	}
-    if x == 0 {
-        log.Printf("Content of Page %s is not updated", page.Url)
-    }
+	if x == 0 {
+		log.Printf("Content of Page %s is not updated", page.Url)
+	}
 
 	return nil
+}
+
+func getFeedBySlug(db *sql.DB, slug string) (Feed, error) {
+	var feed Feed
+	row := db.QueryRow(`
+        SELECT id, slug, url, created_at, updated_at, refresh_ms
+        FROM feeds
+        WHERE slug = ?
+        LIMIT 1
+        ;
+    `, slug)
+	err := row.Scan(
+		&feed.ID,
+		&feed.Slug,
+		&feed.Url,
+		&feed.CreatedAt,
+		&feed.UpdatedAt,
+		&feed.RefreshMs,
+	)
+	if err != nil {
+		return Feed{}, err
+	}
+	return feed, nil
 }
 
 func findFeedByUrl(db *sql.DB, feedUrl string) (Feed, error) {
@@ -433,6 +458,46 @@ func getAllPages(db *sql.DB) ([]Page, error) {
 	return result, nil
 }
 
+func getFeedRecords(db *sql.DB, feedId string, mdContent bool) ([]Record, error) {
+    log.Print("[WARN] mdContent is not implemented")
+	result := make([]Record, 0)
+	rows, err := db.Query(`
+        SELECT
+            id,
+            title,
+            description,
+            content,
+            published_at,
+            link
+        FROM records
+        WHERE feed_id = ?
+        ;
+    `, feedId)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var rec Record
+		err := rows.Scan(
+			&rec.ID,
+			&rec.Title,
+			&rec.Description,
+			&rec.Content,
+			&rec.PublishedAt,
+			&rec.Link,
+		)
+		if err != nil {
+			log.Printf("Failed to get row: %v", err)
+			continue
+		}
+		result = append(result, rec)
+	}
+
+	return result, nil
+}
+
 func createJsonFeed() {
 	// Select record
 	// var selectedRecord Record
@@ -512,25 +577,96 @@ func allToMd(db *sql.DB) {
 	}
 
 	for _, page := range pages {
-        html := strings.NewReader(page.Html)
+		html := strings.NewReader(page.Html)
 		a, err := r.Parse(html, page.Url)
 		if err != nil {
 			log.Printf("Readability failed to parse %s: %v", page.Url, err)
-            continue
+			continue
 		}
 
-        md, err := htmlToMd(a.Content)
-        if err != nil {
+		md, err := htmlToMd(a.Content)
+		if err != nil {
 			log.Printf("Markdown failed to convert from html %s: %v", page.Url, err)
-            continue
-        }
+			continue
+		}
 
-        err = updatePageContent(db, &page, md)
-        if err != nil {
+		err = updatePageContent(db, &page, md)
+		if err != nil {
 			log.Printf("Failed to update content of page %s: %v", page.Url, err)
-            continue
-        }
+			continue
+		}
 	}
+}
+
+func serve(db *sql.DB) {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	app.Get("/feed/:slug", func(c *fiber.Ctx) error {
+		slug := c.Params("slug")
+		feed, err := getFeedBySlug(db, slug)
+		if err != nil {
+			return c.Status(404).JSON(&fiber.Map{
+				"error": "Feed not found",
+			})
+		}
+
+		records, err := getFeedRecords(db, feed.ID, true)
+		if err != nil {
+			return c.Status(404).JSON(&fiber.Map{
+				"error": "Records not found",
+			})
+		}
+
+		items := make([]*jsonfeed.Item, 0)
+		for _, rec := range records {
+			item := jsonfeed.Item{
+				ID:  rec.ID,
+				URL: rec.Link,
+				Title: rec.Title,
+				// ContentHTML   string  `json:"content_html,omitempty"`   // content_html and content_text are each optional strings — but one or both must be present. This is the HTML or plain text of the item. Important: the only place HTML is allowed in this format is in content_html. A Twitter-like service might use content_text, while a blog might use content_html. Use whichever makes sense for your resource. (It doesn’t even have to be the same for each item in a feed.)
+				ContentText: rec.Content,
+				Summary:     rec.Description,
+				// Image         string  `json:"image,omitempty"`          // image (optional, string) is the URL of the main image for the item. This image may also appear in the content_html
+				// BannerImage   string  `json:"banner_image,omitempty"`   // banner_image (optional, string) is the URL of an image to use as a banner.
+				DatePublished: rec.PublishedAt.String(),
+				// DateModified  string  `json:"date_modified,omitempty"`  // date_modified (optional, string) specifies the modification date in RFC 3339 format.
+				// Author        *Author `json:"author,omitempty"`         // author (optional, object) has the same structure as the top-level author. If not specified in an item, then the top-level author, if present, is the author of the item.
+
+				Tags: []string{"good", "trash", "travel"},
+				// Attachments *[]Attachments `json:"attachments,omitempty"` // attachments (optional, array) lists related resources. Podcasts, for instance, would include an attachment that’s an audio or video file. An individual item may have one or more attachments.
+				// Authors  []*Author `json:"authors,omitempty"`
+				// Language string    `json:"language,omitempty"`
+			}
+			items = append(items, &item)
+		}
+
+		url := fmt.Sprintf("http://127.0.0.1:3000/feed/%s", slug)
+		f := jsonfeed.Feed{
+			Version: "https://jsonfeed.org/version/1.1",
+			Title: feed.Slug,
+			// HomePageURL string  `json:"home_page_url,omitempty"` // home_page_url (optional but strongly recommended, string) is the URL of the resource that the feed describes. This resource should be an HTML page
+			FeedURL: url,
+			// Description string  `json:"description,omitempty"`   // description (optional, string)
+			// UserComment string  `json:"user_comment,omitempty"`  // user_comment (optional, string) is a description of the purpose of the feed. This is for the use of people looking at the raw JSON, and should be ignored by feed readers.
+			// NextURL     string  `json:"next_url,omitempty"`      // next_url (optional, string) is the URL of a feed that provides the next n items. This allows for pagination
+			// Icon        string  `json:"icon,omitempty"`          // icon (optional, string) is the URL of an image for the feed suitable to be used in a timeline. It should be square and relatively large — such as 512 x 512
+			// Favicon     string  `json:"favicon,omitempty"`       // favicon (optional, string) is the URL of an image for the feed suitable to be used in a source list. It should be square and relatively small, but not smaller than 64 x 64
+			// Author      *Author `json:"author,omitempty"`        // author (optional, object) specifies the feed author. The author object has several members. These are all optional — but if you provide an author object, then at least one is required:
+			// Expired     bool    `json:"expired,omitempty"`       // expired (optional, boolean) says whether or not the feed is finished — that is, whether or not it will ever update again.
+			Items: items,
+			// Authors  []*Author `json:"authors,omitempty"`
+			// Language string    `json:"language,omitempty"`
+		}
+		return c.JSON(f)
+	})
+
+	log.Print("Listening :3000")
+	err := app.Listen(":3000")
+	log.Fatal(err)
 }
 
 func main() {
@@ -553,6 +689,8 @@ func main() {
 	// err = importOpml(db, "20230426-reeder.opml")
 	// err = addFeed(db, "hacker-news", feedUrl)
 
+	go serve(db)
+
 	feeds, err := getFeeds(db)
 	if err != nil {
 		log.Fatal(err)
@@ -568,7 +706,7 @@ func main() {
 		go handleRecords(db, news)
 	}
 	go handleOldRecords(db, news)
-    // go allToMd(db)
+	// go allToMd(db)
 
 	for _, feed := range feeds {
 		go runFeed(db, feed, news)
